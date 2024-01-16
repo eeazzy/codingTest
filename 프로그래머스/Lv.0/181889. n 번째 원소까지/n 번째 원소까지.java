adf4d16a7dc9aedb152563c556bc6a739a9a8d1c
김현서 <143472229+eeazzy@users.noreply.github.com>
2024-01-16 19:59:04 +0900

    [level Lv.0] Title: n 번째 원소까지, Time: 0.01 ms, Memory: 70.7 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181889. n 번째 원소까지/n 번째 원소까지.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        int[] answer = {};
+        int result [] = new int[n];
+        
+        for(int i=0;i<n;i++){
+            result[i] = num_list[i];
+        }
+        
+        answer = result;
+        return answer;
+    }
+}
